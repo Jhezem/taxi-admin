576c64273f14cbebefdddcb8fb2c7253a270991f
Jhezem <mmartinhez08@gmail.com>
2025-07-05 16:04:07 -0600

    fix: :rotating_light: linter issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import { EntryForm } from "@/components/entry-form";
 import LastMovements from "@/components/last-movements";
-import Header from "@/components/header";
 import BalanceCard from "@/components/balance-card";
-import Footer from "@/components/footer";
 import PullToRefreshComponent from "../components/pull-to-refresh";
 import { Fragment } from "react";
 
